e2d592a9336468c583c8a3e96f9e81f654b030aa
Nicolas Galano <nicolas.galano@gmail.com>
2018-10-30 02:37:27 -0300

    First Dev

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -37,6 +37,11 @@ define('DB_CHARSET', 'utf8mb4');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
+define('FS_METHOD','direct');
+define("FTP_HOST", "localhost");
+define("FTP_USER", "adminpapyrus");
+define("FTP_PASS", "!8HPu2wuQoSKwsL1Me");
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
